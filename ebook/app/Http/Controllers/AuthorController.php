a5d03f20f0b283deb7580bfa728169e9dabd2757
noviadhyacamila <novialemon19@gmail.com>
2022-10-31 09:47:03 +0700

    challenge author

--- ebook/app/Http/Controllers/AuthorController.php
+++ ebook/app/Http/Controllers/AuthorController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Book;
+use App\Models\Author;
 
-class BookController extends Controller
+class AuthorController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,8 +14,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        $data = Book::all();
-        return $data;
+        $author = author::all();
+        return $author;
     }
 
     /**
@@ -36,18 +36,19 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        $table = Book::create([
-            "title" => $request->title,
-            "description" => $request->description,
-            "author" => $request->author,
-            "publisher" => $request->publisher,
-            "date_of_issue" => $request->date_of_issue
+        $author = author::create([
+            "name" => $request->name,
+            "date_of_birth" => $request->date_of_birth,
+            "place_of_birth" => $request->place_of_birth,
+            "gender" => $request->gender,
+            "email" => $request->email,
+            "hp" => $request->hp
         ]);
 
         return response()->json([
             'success' => 201,
             'message' => 'Data berhasil disimpan',
-            'data' => $table
+            'data' => $author
         ], 201);
     }
 
@@ -59,11 +60,11 @@ class BookController extends Controller
      */
     public function show($id)
     {
-        $book = book::find($id);
-        if ($book) {
+        $author = author::find($id);
+        if ($author) {
             return response()->json([
                 'status' => 200,
-                'data' => $book
+                'data' => $author
 
             ], 200);
         } else {
@@ -94,17 +95,19 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $book = book::find($id);
-        if($book){
-            $book->title = $request->title ? $request->title : $book->title;
-            $book->description = $request->description ? $request->desription : $book->description;
-            $book->author = $request->author ? $request->author : $book->author;
-            $book->publisher = $request->publisher ? $request->publisher : $book->publisher;
-            $book->date_of_issue = $request->date_of_issue ? $request->date_of_issue : $book->date_of_issue;
-            $book->save();
+        $author = author::find($id);
+        if($author){
+            $author->name = $request->name ? $request->name : $author->name;
+            $author->date_of_birth = $request->date_of_birth ? $request->date_of_birth : $author->date_of_birth;
+            $author->place_of_birth = $request->place_of_birth ? $request->place_of_birth : $author->place_of_birth;
+            $author->gender = $request->gender ? $request->gender : $author->gender;
+            $author->email = $request->email ? $request->email : $author->email;
+            $author->hp = $request->hp ? $request->hp : $author->hp;
+            $author->save();
+
             return response()->json([
                 'status' => 200,
-                'data' => $book
+                'data' => $author
             ],200);
 
         }else{
@@ -123,12 +126,12 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        $book = book::where('id',$id)->first();
-        if($book){
-            $book->delete();
+        $author = author::where('id',$id)->first();
+        if($author){
+            $author->delete();
             return response()->json([
                 'status' =>200,
-                'data'=> $book
+                'data'=> $author
             ],200);
         }else{
             return response()->json([
