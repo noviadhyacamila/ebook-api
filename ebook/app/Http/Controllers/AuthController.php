66bbff9ad10e5abf5fe4336bc238b9dca2d96e5c
noviadhyacamila <novialemon19@gmail.com>
2022-11-01 14:51:53 +0700

    auth

--- ebook/app/Http/Controllers/AuthController.php
+++ ebook/app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,6 +14,66 @@ class AuthController extends Controller
             'Name'  => 'Novia Dhya Camila',
             'Phone' => '085741014350',
             'Class' => 'XII RPL 5'
+        ];
+    }
+
+    public function register(Request $request){
+
+        $field = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $field['name'],
+            'email' => $field['email'],
+            'password' => bcrypt($field['password']),
+        ]);
+
+        $token = $user->CreateToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $field = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $field['email'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($field['password'], $user->password)) {
+            return response([
+                'massage' => 'unauthorized'
+            ], 401);
+        }
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'massage' => 'Logged out'
         ];
     }
 }
--- ebook/routes/api.php
+++ ebook/routes/api.php
@@ -20,19 +20,25 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// public route
+Route::post('register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('me', [AuthController::class, 'me']);
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [BookController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
 
-//Route::get('books', [BookController::class, 'index']);
-//Route::get('books/{id}', [BookController::class, 'show']);
+
 //Route::post('books', [BookController::class, 'store']);
 //Route::put('books/{id}', [BookController::class, 'update']);
 //Route::delete('books/{id}', [BookController::class, 'destroy']);
 
-Route ::resource('books', BookController::class)->except(
-    ['create', 'edit']
-);
 
-Route ::resource('authors', AuthorController::class)->except(
-    ['create', 'edit']
-);
+
+// protected routes (yang melewati sanctum)
+Route::middleware('auth:sanctum')->group(function (){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
